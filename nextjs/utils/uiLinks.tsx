e98e2cddb9ee0e128563ed1a2f3dd040af68a6b1
SonDH <sondh0127@gmail.com>
2020-08-21 17:32:56 +0700

    fix icon

--- nextjs/utils/uiLinks.tsx
+++ nextjs/utils/uiLinks.tsx
@@ -7,17 +7,8 @@ const links: Array<{
     link: 'dashboard',
     name: 'Dashboard',
     icon: (
-      <svg
-        className="w-5 h-5"
-        aria-hidden="true"
-        fill="none"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
+      <svg viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 home">
+        <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
       </svg>
     ),
   },
